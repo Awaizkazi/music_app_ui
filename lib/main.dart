92e93f29a75a6d4b06e02da9a6044a5cf2a5af53
Awaizkazi <Owaizkazi789@gmail.com>
2023-02-22 11:10:10 +0530

    c

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:music_app_ui/screens/home_screen.dart';
+import 'package:music_app_ui/screens/song_screen.dart';
+import 'screens/playlist_screen.dart';
+
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Music App',
+      theme: ThemeData(primaryColor: Colors.blue),
+      home: HomeScreen(),
+      getPages: [
+        GetPage(name: '/', page: () => HomeScreen()),
+        GetPage(name: '/song', page: () => SongScreen()),
+        GetPage(name: '/playlist', page: () => PlayListScreen()),
+        GetPage(name: '/', page: () => HomeScreen()),
+      ],
+    );
+  }
+}
+
+
+/*
+https://www.youtube.com/watch?v=12ktVNkKw5w
+2:00 Se Dekna hai
+*/
