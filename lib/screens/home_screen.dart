367fc66d0392a4cf46517a5f864b76384e01f3ef
Awaizkazi <Owaizkazi789@gmail.com>
2023-02-22 14:44:54 +0530

    c

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../models/song_model.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -10,6 +12,7 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    List<Song> songs = Song.songs;
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
@@ -30,10 +33,48 @@ class _HomeScreenState extends State<HomeScreen> {
             children: [
               //! Here we are calling the Cusotmize Search bar
               _DiscoverMusic(),
+              Column(
+                children: [
+                  SectionHeader(title: 'Trending Music'),
+                ],
+              ),
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class SectionHeader extends StatelessWidget {
+  const SectionHeader({
+    super.key,
+    required this.title,
+    this.action = 'View More',
+  });
+  final String title;
+  final String action;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          'Trending Music',
+          style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+        ),
+        Text(
+          'View More',
+          style: Theme.of(context)
+              .textTheme
+              .bodyLarge!
+              .copyWith(color: Colors.white),
+        ),
+      ],
     );
   }
 }
