10b64df99bd6ecd95fda583058a8c5f9afebd0b3
Awaizkazi <Owaizkazi789@gmail.com>
2023-02-23 13:03:11 +0530

    SeekBar Done

--- lib/widgets/seekbar.dart
+++ lib/widgets/seekbar.dart
@@ -12,7 +12,7 @@ class SeekBar extends StatefulWidget {
   final Duration duration;
   final ValueChanged<Duration>? onChanged;
   final ValueChanged<Duration>? onChangedEnd;
-  
+
   const SeekBar(
       {super.key,
       required this.position,
@@ -27,6 +27,26 @@ class SeekBar extends StatefulWidget {
 class _SeekBarState extends State<SeekBar> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: [
+        Expanded(
+          child: SliderTheme(
+            data: SliderTheme.of(context).copyWith(
+              trackHeight: 4,
+              thumbShape: RoundSliderThumbShape(
+                disabledThumbRadius: 4,
+                enabledThumbRadius: 4,
+              ),
+              overlayShape: RoundSliderOverlayShape(overlayRadius: 10),
+              activeTrackColor: Colors.white.withOpacity(0.2),
+              inactiveTickMarkColor: Colors.white,
+              thumbColor: Colors.white,
+              overlayColor: Colors.white,
+            ),
+            child: Slider(value: 0.0, onChanged: (value) {}),
+          ),
+        ),
+      ],
+    );
   }
 }
